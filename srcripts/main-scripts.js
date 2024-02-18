4b8f03d33001e4171907235b3d63d3a020308913
maab-fw <maabfw@proton.me>
2024-02-18 15:36:07 +0600

    seat selection and display function completed

--- srcripts/main-scripts.js
+++ srcripts/main-scripts.js
@@ -1,5 +1,6 @@
 const allSeats = document.getElementById('allSeats')
 const seatButton = document.getElementsByClassName('seatButton')
+const appendedSection = document.getElementById('appended-section')
 
 let seatCount = 0
 for (const seat of seatButton) {
@@ -10,13 +11,28 @@ for (const seat of seatButton) {
             seat.classList.remove('bg-[#F7F8F8]')
             seat.setAttribute('disabled', '')
 
-            const selectedSeatCount = getInnerTextValueFromId('selected-seat-count')
-            seatCount = selectedSeatCount + 1
+            seatCount = seatCount + 1
             setInnerTextWithIdAndValue('selected-seat-count', seatCount)
 
             const remainingSeat = getInnerTextValueFromId('remaining-seat')
             const updatedRemainingSeat = remainingSeat - 1
             setInnerTextWithIdAndValue('remaining-seat', updatedRemainingSeat)
+
+            const div = document.createElement('div')
+            div.classList.add("my-2", "flex", "justify-between", "text-[rgba(3,7,18,0.6)]")
+
+
+            const p1 = document.createElement('p')
+            const seatText = seat.innerText
+            p1.innerText = seatText;
+
+            const p2 = document.createElement('p')
+            p2.innerText = 'Economy';
+
+            const p3 = document.createElement('p')
+            p3.innerText = 550;
+            div.append(p1, p2, p3)
+            appendedSection.appendChild(div)
         }
         else {
             alert("You can't select more seat!!!")
